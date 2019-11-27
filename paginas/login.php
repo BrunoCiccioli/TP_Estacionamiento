bebc13037b55ff62ba0e79763dec4e0c733495de
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-11-27 14:29:10 -0300

    mod basicas

--- paginas/login.php
+++ paginas/login.php
@@ -34,7 +34,7 @@ session_start();
     <main role="main" class="container">
          
       <h2 class="mt-5">ESTACIONAMIENTO CICCIOLI</h2>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mexico_road_sign_estacionamiento.svg/1024px-Mexico_road_sign_estacionamiento.svg.png" width=50 height=50><img src="https://www.logolynx.com/images/logolynx/cc/cc7870cf8dca735bd9872c4845939519.jpeg" width=200 height=150><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mexico_road_sign_estacionamiento.svg/1024px-Mexico_road_sign_estacionamiento.svg.png" width=50 height=50>
+      <img src="https://www.tiempodesanjuan.com/u/fotografias/fotosnoticias/2019/11/19/580356.jpg" width=100 height=100><img src="https://www.mibelgrano.com.ar/wp-content/uploads/2019/03/Estacionamiento.jpg" width=400 height=300><img src="https://www.tiempodesanjuan.com/u/fotografias/fotosnoticias/2019/11/19/580356.jpg" width=100 height=100>
 
 
 
