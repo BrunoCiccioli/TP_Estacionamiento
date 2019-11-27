5a4682dc8bf0a06a97efd53314e2969ef4503640
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-11-27 14:06:58 -0300

    registro edit

--- funciones/hacerRegistro.php
+++ funciones/hacerRegistro.php
@@ -1,54 +1,31 @@
 <?php
-
 include 'accesoadatos.php';
-//var_dump("hola"); die();
-
-
 $miObjeto = new stdClass();
-//var_dump("hola"); die();
 $miObjeto->nombre = $_GET['nombre'];
-//var_dump("hola"); die();
 $miObjeto->contraseña = $_GET['contraseña'];
-//var_dump("hola"); die();
 
 
-    $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
-	//var_dump("hola"); die();
-
-	$consulta =$objetoAccesoDato->RetornarConsulta("select nombre from usuario");
-	//var_dump("hola"); die();
-	$consulta->execute();
-	//var_dump("hola"); die();			
-	$datos= $consulta->fetchAll(PDO::FETCH_ASSOC);		
-	//var_dump("hola"); die();
-	foreach ($datos as $usuario) 
+$objetoAccesoDato= AccesoDatos::dameUnObjetoAcceso();
+$consulta =$objetoAccesoDato->RetornarConsulta("select nombre from usuario");
+$consulta->execute();
+$datos=$consulta->fetchall(PDO::FETCH_ASSOC);
+//var_dump($datos[0]['nombre']);
+//var_dump($datos);
+foreach ($datos as $usuario) 
+	{
+	if($usuario["nombre"]==$miObjeto->nombre)
 		{
-			if($usuario["nombre"]==$miObjeto->nombre)
-			{
-				header("Location: ../paginas/usuarioyaregistrado.php");
-		        exit();
-			}
-			
+			header("Location:../paginas/usuarioexistente.php");
+			exit();
 		}
-
-
-        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
-        //var_dump("hola"); die();
-        $select="INSERT INTO usuario( nombre, clave) VALUES ('$miObjeto->nombre','$miObjeto->contraseña')";
-        //var_dump("hola"); die();
-        $consulta =$objetoAccesoDato->RetornarConsulta($select);
-        //var_dump("hola"); die();
-        $consulta->execute();
-
-        header("Location: ../paginas/ok.php");
-        exit();
-        //var_dump("hola"); die();
-        
-        
-
-//$archivo = fopen('../archivos/registro.txt', 'a');
-//fwrite($archivo, json_encode($miObjeto)."\n");
-//header("Location: ../paginas/ok.php");
-//fclose($archivo);
-
+	}
+if(isset($datos[0]['nombre']))
+	{
+	$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+	$select="INSERT INTO usuario( nombre, clave) VALUES ('$miObjeto->nombre','$miObjeto->contraseña')";
+	$consulta =$objetoAccesoDato->RetornarConsulta($select);
+	$consulta->execute();
+}
+header("Location: ../paginas/ok.php");
+	
 ?>
