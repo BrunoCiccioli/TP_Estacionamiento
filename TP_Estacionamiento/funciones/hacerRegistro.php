c014fa0ac6b8984e18412bb6bd38dcf170973583
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-11-27 18:15:07 -0300

    Add files via upload

---
+++ TP_Estacionamiento/funciones/hacerRegistro.php
@@ -0,0 +1,52 @@
+<?php
+include 'accesoadatos.php';
+
+
+
+$miObjeto = new stdClass();
+//var_dump("hola"); die();
+$miObjeto->nombre = $_GET['nombre'];
+//var_dump("hola"); die();
+$miObjeto->contraseña = $_GET['contraseña'];
+//var_dump("hola"); die();
+
+
+    $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+	//var_dump("hola"); die();
+
+	$consulta =$objetoAccesoDato->RetornarConsulta("select nombre from usuario");
+	//var_dump("hola"); die();
+	$consulta->execute();
+	//var_dump("hola"); die();			
+	$datos= $consulta->fetchAll(PDO::FETCH_ASSOC);		
+	//var_dump("hola"); die();
+	foreach ($datos as $usuario) 
+		{
+			if($usuario["nombre"]==$miObjeto->nombre)
+			{
+				header("Location: ../paginas/usuarioyaregistrado.php");
+		        exit();
+			}
+			
+		}
+
+
+	//var_dump("hola"); die();
+	$select="INSERT INTO usuario( nombre, clave) VALUES ('$miObjeto->nombre','$miObjeto->contraseña')";
+	//var_dump("hola"); die();
+	$consulta =$objetoAccesoDato->RetornarConsulta($select);
+	//var_dump("hola"); die();
+	$consulta->execute();
+	//var_dump("hola"); die();
+
+header("Location: ../paginas/ok.php");
+	
+        
+        
+
+//$archivo = fopen('../archivos/registro.txt', 'a');
+//fwrite($archivo, json_encode($miObjeto)."\n");
+//header("Location: ../paginas/ok.php");
+//fclose($archivo);
+
+?>
